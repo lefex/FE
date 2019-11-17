8a292dd0f685aed0b522d96410afa35af6ea959b
SuYan Wang <suyanwang@wsy.lan>
2019-11-17 21:05:20 +0800

    https://tools.ietf.org/pdf/rfc793.pdf

--- 第三阶段/app/server.js
+++ 第三阶段/app/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
-const server = new http.server();
+// 创建一个 HTTP server
+const server = new http.Server();
 
 /**
  * request 事件，当客户端发起请求后会响应这个事件
@@ -7,21 +8,33 @@ const server = new http.server();
  * res：响应对象
  * */ 
 server.on('request', function(req, res) {
-    res.writeHead(200, {
-        "Content-type" : "application/json"
-    });
-    let data = {
-        title: "前端小课",
-        des: "内容由素燕公众号发布"
-    };
-    res.write(JSON.stringify(data));
+    let path = req.url;
+    if (path.indexOf('/api/fe/list') == 0) {
+        // 处理请求的 path 为 /api/fe/list
+        res.writeHead(200, {
+            "Content-type" : "application/json"
+        });
+        let data = {
+            title: "前端小课",
+            des: "内容由素燕公众号发布"
+        };
+        // 最终数据需要转换成 json 字符串
+        res.write(JSON.stringify(data));
+    } else {
+        // 未实现，直接报 404 错误
+        res.writeHead(404, {
+            "Content-type" : "application/json"
+        });
+        let data = {
+            code: "404",
+            msg: "not found"
+        };
+        res.write(JSON.stringify(data));
+    }
     res.end();
 });
 
-server.on('conection', function(req, socket, head) {
-
-});
-
+// 监听 8888 端口
 server.listen(8888, function() {
-    console.log('Server run in: 127');
-})
+    console.log('Server run in: http://127.0.0.1:8888');
+});
