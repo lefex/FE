ac6e8f58bd60358eb30f13a8ea4369feda935189
lefex <wsyxyxs@126.com>
2022-03-01 07:55:23 +0800

    数据结构与算法

---
+++ dsa/src/tree/create.ts
@@ -0,0 +1,8 @@
+/**
+ * @author 素燕（我有个公众号：素燕）
+ * @description 创建树，树的数据结构定义
+ */
+
+export function createTree() {
+    console.log('create a tree');
+}
---
+++ dsa/src/tree/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @author 素燕（我有个公众号：素燕）
+ * @description 树相关的算法
+ */
+
+import { createTree } from "./create"
+
+const createDemo = () => {
+    createTree();
+}
+
+createDemo();
